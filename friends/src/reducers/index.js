dda3db3e3eb17c25677059298e1167c965e49941
William Soukkachang <WSoukkachang@yahoo.com>
2019-05-01 22:20:11 -0500

    Initial commit, created Store and wrapped in Provider tag. Working on routing to login page.

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,13 @@
+
+const initialState =  {
+    friends:[],
+    loggingIn: false,
+    error: null
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        default: 
+        return state;
+    }
+}
