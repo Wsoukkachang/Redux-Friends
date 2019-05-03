12966323e8a7e1cfa2306aa3264901e7b34956b2
William Soukkachang <WSoukkachang@yahoo.com>
2019-05-02 22:24:36 -0500

    Built Actions and Login Components. Added Routes. Still need work on login and fetch funtionality.

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { getFriends } from '../actions';
+
+class FriendsList extends Component {
+
+    componentDidMount() {
+        localStorage.setItem("token", "token")
+        this.props.getFriends();
+    }
+    
+    render() {
+        console.log("friends", this.props.friends)
+        return (
+            <div>
+            <h2>These are your friends</h2>
+            {this.props.friends.map(friend => <>{friend}</>)}
+            </div>
+            )
+        }
+    }
+
+const mapStateToProps = state => {
+    return {
+        friends: state.friends
+    }
+}
+
+export default connect(mapStateToProps, {getFriends})(FriendsList); 
